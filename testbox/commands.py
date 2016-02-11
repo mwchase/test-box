3c09f17be64c83613df1b24504b7329a538df554
Max Chase <max.chase@gmail.com>
2016-02-10 20:10:21 -0500

    Well, it's something.

--- testbox/commands.py
+++ testbox/commands.py
@@ -1,25 +1,41 @@
+import os
 import subprocess
 import sys
 
 
-class Runner(object):
-    pass
+def python_module(args):
+    subprocess.call((sys.executable, '-m') + args)
 
 
-class Python(Runner):
-    pass
+COVER = 'coverage'
+PYTEST = 'py.test'
 
 
-class Module(object):
-
-    name = None
-    python = sys.executable,
-    module = '-m',
-
-    @classmethod
-    def _run(cls, args):
-        my_args = cls.python + cls.module + cls.name + cls.args
-        subprocess.call(my_args)
+def initial_package(package):
+    python_module((COVER, 'run', '--source={}'.format(package),
+                   '{}/__init__.py'.format(package)))
 
 
+def erase():
+    python_module((COVER, 'erase'))
 
+
+def html():
+    python_module((COVER, 'html'))
+
+
+def dotted_module(module):
+    return '.'.join(module)
+
+
+def slashed_module(module):
+    return os.sep.join(module)
+
+
+def run(package, module, test='test', results='results',
+        aggregate='aggregate-results.xml'):
+    python_module(COVER, 'run', '-a',
+                  '--source={}.{}'.format(package, dotted_module(module)),
+                  '-m', PYTEST, '--junit-xml={}/{}.xml'.format(
+                      results, dotted_module(module)),
+                  '{}/{}'.format(test, slashed_module(module)))
